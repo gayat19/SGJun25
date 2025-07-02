c74e6b5b022417c27a3a0a81923d0289a1b4432b
unknown <gayat_19@yahoo.com>
2025-07-02 15:50:19 +0530

    useMemo

--- Day8/my-app/src/Components/Home/Home.tsx
+++ Day8/my-app/src/Components/Home/Home.tsx
@@ -2,17 +2,29 @@ import { Button } from "@mui/material";
 import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../Contextx/AuthContext";
 import AccountMenu from "../AccountMenu";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import One from "../ONe";
 import Two from "../Two";
 
+
 const Home = ()=>{
     const [check,setCheck] = useState(true);
+    const [number,setNumebr] = useState(100);
+    const calculate = useMemo(()=>{
+        console.log('Use Memo function triggerd');
+        const compute = (num:number)=>{
+            return num+num+1*3/num *num;
+        }
+       return compute(number);
+    },[number])
     const navigate=useNavigate();
        const {user,logout} = useAuth();
+    //const result = calculate(number)
     return(<>
     <AccountMenu/>
+    {calculate}
     <Button onClick={()=>setCheck(!check)} color="info">Toggle</Button> 
+    <button onClick={()=>setNumebr(number+1)}>Increment</button>
     <One/>
     <hr/>
     <Two/>
